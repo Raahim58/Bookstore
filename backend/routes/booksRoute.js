11571e4914611654ff2b521e4cdced830ba22024
Raahim58 <raahimabdulsamad@gmail.com>
2024-06-27 00:16:45 +0500

    pages functionality completed

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -25,7 +25,7 @@ router.post('/', async (req, res) => {
         return res.status(201).send(book);
     } catch (error) {
         console.log(error.message);
-        response.status(500).send({ message: error.message})
+        res.status(500).send({ message: error.message})
     }
 })
 
